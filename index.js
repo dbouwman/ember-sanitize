03f0eaefcb133d5db9e2bef844bb4b25075d6745
Dave Bouwman <dave@davebouwman.com>
2019-04-24 10:16:39 -0600

    clean up index.js

--- index.js
+++ index.js
@@ -5,15 +5,7 @@ module.exports = {
   name: 'ember-sanitize',
 
   included: function(app /* parentAddon */) {
-    // var target = (parentAddon || app);
-    // this._super.included(target);
-    // target.import('bower_components/sanitize.js/lib/sanitize.js');
-    // var current = this;
-    // do {
-    //   app = current.app || app;
-    // } while (current.parent.parent && (current = current.parent));
-    // app.import(app.bowerDirectory + '/sanitize.js/lib/sanitize.js');
-    app.import('vendor/sanitize/sanitize.js');
+    app.import('vendor/sanitize.js/sanitize.js');
   }
 
 };
