4d27fc0216c69f63df2a533fdd59d1bf039fcc3d
Dave Bouwman <dave@davebouwman.com>
2019-04-24 09:54:44 -0600

    remove bower.json, add sanitize.js into vendor

--- index.js
+++ index.js
@@ -4,15 +4,16 @@
 module.exports = {
   name: 'ember-sanitize',
 
-  included: function(app, parentAddon) {
+  included: function(app /* parentAddon */) {
     // var target = (parentAddon || app);
     // this._super.included(target);
     // target.import('bower_components/sanitize.js/lib/sanitize.js');
-    var current = this;
-    do {
-      app = current.app || app;
-    } while (current.parent.parent && (current = current.parent));
-    app.import(app.bowerDirectory + '/sanitize.js/lib/sanitize.js');
+    // var current = this;
+    // do {
+    //   app = current.app || app;
+    // } while (current.parent.parent && (current = current.parent));
+    // app.import(app.bowerDirectory + '/sanitize.js/lib/sanitize.js');
+    app.import('vendor/sanitize/sanitize.js');
   }
 
 };
